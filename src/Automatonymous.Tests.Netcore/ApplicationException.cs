870a451b4fae50aacf92416d37372f6a1122ab04
Alexander Hoglind <alexander.hoglind@lex247.com>
2016-10-10 14:27:52 +0700

    #3 Added a new ApplicationException class to Tests.Netcore project since it doesn't exists in Netcore

---
+++ src/Automatonymous.Tests.Netcore/ApplicationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Automatonymous.Tests
+{
+  public class ApplicationException : Exception {
+    public ApplicationException() : base() {
+
+    }
+
+    public ApplicationException(string message) : base(message) {
+
+    }
+
+    public ApplicationException(string message, Exception innerException) : base(message, innerException) {
+
+    }
+  }
+}
